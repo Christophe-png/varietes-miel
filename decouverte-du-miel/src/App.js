e43d5455311aabd1d1bec618648e0ddb6c063e56
CASTAN <christophecastan4@gmail.com>
2020-12-14 18:03:30 +0100

    add props

--- decouverte-du-miel/src/App.js
+++ decouverte-du-miel/src/App.js
@@ -2,19 +2,33 @@ import React, { Component } from "react";
 import Header from "./Components/Header";
 import HomePage from "./Components/HomePage";
 import ListeMiels from "./Components/ListeMiels";
+import axios from "axios";
 //import { Link, Route } from "react-router-dom";
 
 class App extends Component {
   state = {
+    // miels => clé,
+    // []    => valeur
     miels: [],
   };
-
+  componentDidMount() {
+    axios
+      .get("https://my-json-server.typicode.com/Christophe-png/api-miels/miels")
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ miels: data });
+      });
+    console.log("mount");
+  }
   render() {
+    const { miels } = this.state;
+
+    console.log("render", miels);
     return (
       <div className="App">
         <Header />
         <HomePage />
-        <ListeMiels />
+        <ListeMiels miels={miels} />
       </div>
     );
   }
